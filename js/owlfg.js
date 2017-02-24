50136a9f6ac09c7c22cd80960315aa64736b28a5
ThePotatoGuy <andreponce@null.net>
2017-02-23 20:05:34 -0800

    added js workaround to get submit button posting when the form is in modal

--- js/owlfg.js
+++ js/owlfg.js
@@ -2,6 +2,7 @@
 
 $(document).ready(function(){
     $("#submit-to-database").click(function(){
-      $('#results-table').append('<tr id="found-player"><td style=width:25%>name</td> <td style=width:10%>server</td> <td style=width:10%>SR rank</td> <td style=width:10%>role</td> <td style=width:10%>level</td> <td style=width:10%>platform</td> <td style=width:10%>mic</td></tr>');
+//      $('#results-table').append('<tr id="found-player"><td style=width:25%>name</td> <td style=width:10%>server</td> <td style=width:10%>SR rank</td> <td style=width:10%>role</td> <td style=width:10%>level</td> <td style=width:10%>platform</td> <td style=width:10%>mic</td></tr>');
+    document.getElementById("player-add-form").submit();
   });
 });
