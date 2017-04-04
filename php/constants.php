88927e8770895c51503f5e2cb1950615da8267b6
ThePotatoGuy <andreponce@null.net>
2017-04-04 14:20:20 -0700

    fixed cookies syntax erros

--- php/constants.php
+++ php/constants.php
@@ -11,4 +11,38 @@
 // CONSTANTS:
 $SR_DEFAULT = 0;
 $LVL_DEFAULT = 1;
+
+$servers = [
+    0 => "NA",
+    1 => "EU",
+    2 => "AS",
+];
+
+$languages = [
+    0   => "English",
+    1   => "Español(EU)",
+    2   => "Español(AL)",
+    3   => "Italiano",
+    4   => "Русский",
+    5   => "Polski",
+    6   => "Français" ,
+    7   => "Deutsch",
+    8   => "Português",
+    9   => "日本語",
+    10  => "한국어",
+    11  => "简体中文",
+    12  => "繁體中文",
+];
+
+$roles = [
+    0 => "DPS",
+    1 => "Tank",
+    2 => "Support",
+];
+
+$platforms = [
+    0 => "XBOX",
+    1 => "PS4",
+    2 => "PC",
+];
 ?>
